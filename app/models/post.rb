b4b133c04662f4404c663f3c66a7b2b72226ebc7
Keith Isham <kisham8899@gmail.com>
2020-01-21 11:46:43 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+
+  before_save :make_title_case
 
   private
 
@@ -10,6 +13,11 @@ class Post < ActiveRecord::Base
       errors.add(:title, "Title must be in title case")
     end
   end
+
+  def email_author_about_post
+   # Not implemented.
+   # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
+ end
 
   def make_title_case
     self.title = self.title.titlecase
